7b1061d0fedffca665e2bc2975adac363ff8f497
monte1003 <114124291+monte1003@users.noreply.github.com>
2026-04-08 16:06:37 +0000

    Create of function insert_at_end and intert_at_beginning

--- LinkedList.py
+++ LinkedList.py
@@ -1,3 +1,23 @@
+from Node import Node
 class LinkedList:
-    def __init__(self, head):
+    def __init__(self):
         self.head = none
+
+    def insert_at_beginning(self, data):
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
+
+    def insert_at_end(self, data):
+        new_node = Node(data)
+
+        if self.head is None:
+            self.head = new_node
+            return
+        
+        current = self.head
+        while current.next is not None
+            current = current.next
+        
+        current.next = new_node
+    
